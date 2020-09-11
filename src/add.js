9b086751279927d4ba334fab6d4d732169fb86a5
Lachlan McCaskill <101627963@student.swin.edu.au>
2020-09-11 17:59:28 +1000

    CHANGE: Started Javascript page functionality (still getting used to using javascript again)

---
+++ src/add.js
@@ -0,0 +1,12 @@
+window.addEventListener("mouseover", calculateProductTotal);
+window.addEventListener("mouseover", calculateSalesTotal);
+
+function calculateProductTotal()
+{
+    
+}
+
+function calculateSalesTotal()
+{
+
+}
