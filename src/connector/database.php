e60fb982d9b44e0a6017b4a12b007d082b704855
Chuong <chuong.git@gmail.com>
2020-09-22 00:09:50 +1000

    add database object and record object

---
+++ src/connector/database.php
@@ -0,0 +1,31 @@
+<?php
+    class Database {
+        private $dbConnection = null;
+
+        public function __construct()
+        {
+            $host   = "mysql";
+            $db     = "SREPS";
+            $user   = "admin";
+            $password = "password";
+            $charset = "utf8mb4";
+            try {
+                
+                $this->dbConnection = new PDO(
+                    "mysql:host=$host;charset=$charset;dbname=$db",
+                    $user,
+                    $password
+                );
+            }
+            catch(PDOException $e)
+            {
+                exit($e->getMessage());
+            }
+        }
+
+        public function getConnection()
+        {
+            return $this->dbConnection;
+        }
+    }
+?>
---
+++ src/connector/recordGateway.php
@@ -0,0 +1,118 @@
+<?php
+    class SalesRecord {
+        private $connection = null;
+        private $table_name = "SalesRecords";
+
+        public function __construct($db)
+        {
+            $this->connection = $db;
+        }
+
+        public function findAll()
+        {
+            $statement = "
+                SELECT SalesRecordNumber, SalesDate, Comment
+                FROM $table_name
+            ";
+
+            try {
+                $statement = $this->db->query($statement);
+                $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+                return $result;
+            }
+            catch(PDOException $e)
+            {
+                exit($e->getMessage());
+            }
+        }
+
+        public function find($recordNumber)
+        {
+            $statement = "
+                SELECT 
+                    SalesRecordNumber, SalesDate, Comment
+                FROM $table_name
+                WHERE 
+                    SalesRecordNumber = ?;
+            ";            
+
+            try {
+                $statement = $this->db->prepare($statement);
+                $statement->execute(array($recordNumber));
+                $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+                return $result;
+            }
+            catch(PDOException $e)
+            {
+                exit($e->getMessage());
+            }
+        }
+
+        public function insert(Array $input)
+        {
+            $statement = "
+                INSERT INTO $table_name
+                    (SalesRecordNumber, SalesDate, Comment)
+                VALUES
+                    (:SalesRecordNumber, :SalesDate, :Comment);
+            ";
+
+            try {
+                $statement = $this->db->prepare($statement);
+                $statement->execute(array(
+                    'SalesRecordNumber' => $input['SalesRecordNumber'],
+                    'SalesDate' => $input['SalesDate'],
+                    'Comment' => $input['Comment'] ?? null,
+                ));
+
+                return $statement->rowCount();
+            } 
+            catch(PDOException $e)
+            {
+                exit($e->getMessage());
+            }
+        }
+
+        public function update($salesRecordNumber, Array $input)
+        {
+            $statement = "
+                UPDATE $table_name
+                SET
+                    SalesDate = :SalesDate,
+                    Comment = :Comment
+                WHERE SalesRecordNumber = :SalesRecordNumber;
+            ";
+            
+            try {
+                $statement = $this->db->prepare($statement);
+                $statement->execute(array(
+                    'SalesRecordNumber' => $salesRecordNumber,
+                    'SalesDate' => $input['SalesDate'],
+                    'Comment' => $input['Comment'] ?? null,
+                ));
+                return $statement->rowCount();
+            }
+            catch(PDOException $e)
+            {
+                exit($e->getMessage());
+            }
+        }
+
+        public function delete($salesRecordNumber)
+        {
+            $statement = "
+                DELETE FROM $table_name
+                WHERE SalesRecordNumber = ?
+            ";
+
+            try {
+                $statement = $this->db->prepare($statement);
+                $statement->execute(array($salesRecordNumber));
+                return $statement->rowCount();
+            }
+            catch(PDOException $e)
+            {
+                exit($e->getMessage());
+            }
+        }
+    }
