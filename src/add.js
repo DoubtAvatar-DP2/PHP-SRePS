17655dea94bb21bb98a1e2cb864227343e8062df
Lachlan McCaskill <101627963@student.swin.edu.au>
2020-09-21 15:38:16 +1000

    NEW: Added javascript function to calculate sales total

--- src/add.js
+++ src/add.js
@@ -3,15 +3,22 @@ document.getElementById("price").addEventListener("click", calculateProductTotal
 
 function calculateProductTotal()
 {
+    document.getElementById("totalprice").value = "";
+    document.getElementById("total").innerHTML = "Total $0"
     var quantity = document.getElementById("quantity").value;
     var price = document.getElementById("price").value;
-    if (price != "" && quantity != "")
+    if (!isNaN(price) && !isNaN(quantity) && price > 0 && quantity > 0)
     {
         document.getElementById("totalprice").value = quantity*price;
+        calculateSalesTotal();
     }
 }
 
 function calculateSalesTotal()
 {
-
+    var totals = document.getElementById("totalprice").value;
+    if (totals != "")
+    {
+        document.getElementById("total").innerHTML = "Total $" + totals;
+    }
 }
