828b9b21f1f08917aff1852072fd17970a0e207c
Lachlan McCaskill <101627963@student.swin.edu.au>
2020-09-22 14:14:17 +1000

    CHANGE: Got total calculation working for dynamically created rows. Now I just have to fix the total value for the order.

--- src/add.js
+++ src/add.js
@@ -1,44 +1,17 @@
-document.getElementById("quantity").addEventListener("click", calculateProductTotal); 
-document.getElementById("price").addEventListener("click", calculateProductTotal); 
+var inputid = 0;
 
-var inputid = 1;
-
-//addNewProductField();
-//inputid++
-//addNewProductField();
-
-function calculateProductTotal()
-{
-    document.getElementById("totalprice").value = "";
-    document.getElementById("total").innerHTML = "Total $0"
-    var quantity = document.getElementById("quantity").value;
-    var price = document.getElementById("price").value;
-    if (!isNaN(price) && !isNaN(quantity) && price > 0 && quantity > 0)
-    {
-        document.getElementById("totalprice").value = quantity*price;
-        calculateSalesTotal();
-    }
-}
-
-function calculateSalesTotal()
-{
-    var totals = document.getElementById("totalprice").value;
-    if (!isNaN(totals))
-    {
-        document.getElementById("total").innerHTML = "Total $" + totals;
-    }
-}
+addNewProductField();
+addNewProductField();
 
 function addNewProductField()
 {
+    inputid++;
+
     var quantityId = "quantity" + inputid;
     var priceId = "price" + inputid;
     var productNameId = "productname" + inputid;
     var totalpriceId = "totalprice" + inputid;
 
-    //var tablerow = '<tr><td>'+inputid+'</td><td><input type=\"text\" id=\"'+productNameId+'\" name=\"productname\" placeholder=\"Enter product name or ID here\"></td><td><input type=\"number\" id=\"'+quantityId+'\" name=\"quantity\"></td><td><input type=\"number\" id=\"'+priceId+'\" name=\"price\"></td><td><input type=\"number\" id=\"'+totalpriceId+'\" name=\"totalprice\" readonly=\"true\"></td></tr>';
-    
-    //document.getElementById("productEntries").appendChild(tablerow);
     var productEntryTable = document.getElementById("productEntries");
     var productEntryRow = productEntryTable.insertRow(-1);
     var entrynumbercell = productEntryRow.insertCell(0);
@@ -51,9 +24,40 @@ function addNewProductField()
     productNameCell.innerHTML = "<input type=\"text\" id=\""+productNameId+"\" name=\"productname\" placeholder=\"Enter product name or ID here\"></td>"; 
     quantityCell.innerHTML = "<input type=\"number\" id=\""+quantityId+"\" name=\"quantity\">";
     priceCell.innerHTML = "<input type=\"number\" id=\""+priceId+"\" name=\"price\">";
-    totalCell.innerHTML = "<input type=\"number\" id=\""+totalpriceId+"\" name=\"totalprice\" readonly=\"true\">"
-    //write(tablerow);
-    //var row1 = document.createElement('tr');
-    //row1 = '<h1>jjjjjjjjjjjjjjjj</h1>';
-    //row1 = document.getElementById("productEntries").append(row1);
+    totalCell.innerHTML = "<input type=\"number\" id=\""+totalpriceId+"\" name=\"totalprice\" readonly=\"true\">";
+
+    var i = inputid;
+    document.getElementById(quantityId).addEventListener("click", function() {calculateProductTotal(i)}); 
+    document.getElementById(priceId).addEventListener("click", function() {calculateProductTotal(i)}); 
+}
+
+function calculateProductTotal(idNumber)
+{
+    //var idNumber = inputid;
+    var quantityId = "quantity" + idNumber;
+    var priceId = "price" + idNumber;
+    var productNameId = "productname" + idNumber;
+    var totalpriceId = "totalprice" + idNumber;
+
+    document.getElementById(totalpriceId).value = "";
+    document.getElementById("total").innerHTML = "Total $0"
+    var quantity = document.getElementById(quantityId).value;
+    var price = document.getElementById(priceId).value;
+    //alert("Checking values: Quantity: "+quantity+", Price: "+price);
+    if (!isNaN(price) && !isNaN(quantity) && price > 0 && quantity > 0)
+    {
+        document.getElementById(totalpriceId).value = quantity*price;
+        calculateSalesTotal(idNumber);
+    }
+}
+
+function calculateSalesTotal(idNumber)
+{
+    var totalpriceId = "totalprice" + idNumber;
+
+    var totals = document.getElementById(totalpriceId).value;
+    if (!isNaN(totals))
+    {
+        document.getElementById("total").innerHTML = "Total $" + totals;
+    }
 }
