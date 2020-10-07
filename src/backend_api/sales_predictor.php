fbebe76d03d3f3d5a72e978e06545a1e60ca6509
Samuel-Paul-Davis <101093250@student.swin.edu.au>
2020-10-07 13:31:41 +1100

    Fleshed out some of the functions in sales_predictor.php

--- src/backend_api/sales_predictor.php
+++ src/backend_api/sales_predictor.php
@@ -4,11 +4,41 @@
     require "../connector/recordDetail.php";
 
     // functions that will be used 
-    function GetIntercept($tableDataArray);
-    function GetSlope($tableDataArray);
-    function GetXY($tableDataArray);
-    function GetXSquared($tableDataArray);
-    function GetLeastSquareRegression($tableDataArray);
+    function GetIntercept($tableDataArray)
+    {
+
+    }
+
+    function GetSlope($tableDataArray)
+    {
+
+    }
+
+    function GetXY($tableDataArray, $tableDataArrayX)
+    {
+        $dataArrayXY = array();
+
+        // find the appropriate QuantityOrdered and SalesDate, times them together into the array 
+        // and return the array
+    }
+
+    // squares the value of x that's stored in the data array
+    function GetXSquared($tableDataArrayX)
+    {
+        $sqrDataArrayX = array();
+
+        foreach($tableDataArrayX as $table)
+        {
+            array_push($sqrDataArrayX, pow($table["SalesDate"], 2));
+        }
+
+        return $sqrDataArrayX;
+    }
+
+    function GetLeastSquareRegression($tableDataArray)
+    {
+
+    }
 
     /*
     * program starts here
@@ -25,6 +55,9 @@
     // retrieve the salesRecord table (from salesrecord.php)
     // This is needed for the x axis (date of sale)
     $salesRecordTable = new SalesRecord($db);
+
+    // I'm not totally familar with PHP, not sure if this is needed
+    $tableDataArrayX = $recordDetailTabletable->FindAll();
 
     // retrieve the recordDetails table (from recordDetail.php)
     // This is needed for the y axis (number of items sold)
