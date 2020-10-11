2b5b8d1b966592abd9953991190f9571d0c28bd7
Lachlan McCaskill <101627963@student.swin.edu.au>
2020-10-12 10:35:32 +1100

    NEW: Added function to convert product name to ID

--- src/js/recordForm.js
+++ src/js/recordForm.js
@@ -331,6 +331,9 @@ function productToID(productName)
 {
     var productID = 0;
     var productsAndNumbers = JSON.parse(getCookie("productNamesAndNumbers"));
+    productID = productsAndNumbers[productName];
+    document.cookie = "productNamesAndNumbers=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    alert(productID);
     return productID;
 }
 
