7150aa4829f8c3aa21ce10eb53933aa18c2be468
Nick McMahon <nickmcm92@gmail.com>
2020-10-15 15:32:06 +1100

    Created CSV export functions

---
+++ src/connector/export-csv.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Sets the headers for downloading a file
+ *
+ * Sets the Content-Description, type, disposition, etc.
+ *
+ * @param string $fileName The file name to be used
+ * @param int $contentLength the size of the content
+ **/
+function setDownloadHeaders($fileName, $contentLength) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="'.$fileName.'"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . $contentLength);
+}
+
+/**
+ * exportToCSV exports data to CSV
+ *
+ * Intakes an array of data and an optional array of column headings. Generates a CSV and downloads a CSV file
+ *
+ * @param array $data An array of arrays, containing the data to export
+ * @param array $headings default null An optional array containing the column headings
+ **/
+function exportToCSV(array $data, ?array $headings = null) {
+    $output = "";
+
+    if($headings) {
+        foreach ($headings as $heading) {
+            $output .= '"'.$heading.'",';
+        }
+        $output .= "\r\n";
+    }
+
+    foreach ($data as $row) {
+        foreach ($row as $cell) {
+            $output .= '"'.$cell.'",';
+        }
+        $output .= "\r\n";
+    }
+
+    setDownloadHeaders("export.csv", strlen($output));
+
+    flush(); // Flush system output buffer
+    echo $output;
+}
+
+/**
+ * exportAssocToCSV exports data to CSV
+ *
+ * Intakes an associative array of data. Generates a CSV and downloads a CSV file
+ *
+ * @param array $data An array of arrays, containing the data to export
+  **/
+function exportAssocToCSV(array $data) {
+    $output = "";
+
+    //Loop through the first array and use its associative keys as column headings
+    foreach($data[0] as $heading => $cellData) {
+        $output .= '"'.$heading.'",';
+    }
+    $output .= "\r\n";
+    
+    foreach ($data as $key => $row) {
+        foreach ($row as $cell) {
+            $output .= '"'.$cell.'",';
+        }
+        $output .= "\r\n";
+    }
+    setDownloadHeaders("export.csv", strlen($output));
+
+    flush(); // Flush system output buffer
+
+    echo $output;
+}
